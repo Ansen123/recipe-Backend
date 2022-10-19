468af7d4fcd210a1a97e92c3c188b9d751480aed
Ansen123 <ansenfp@gmail.com>
2022-10-19 23:16:30 +0530

    first commit

--- src/main/java/com/nestdigital/Recipe/Controller/RecipeController.java
+++ src/main/java/com/nestdigital/Recipe/Controller/RecipeController.java
@@ -3,10 +3,9 @@ package com.nestdigital.Recipe.Controller;
 import com.nestdigital.Recipe.Dao.RecipeDao;
 import com.nestdigital.Recipe.Model.RecipeModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class RecipeController {
@@ -17,6 +16,11 @@ public class RecipeController {
 public String recipe(@RequestBody RecipeModel recipe){
     dao.save(recipe);
     return "{status:'Success'}";
+}
+@CrossOrigin(origins = "*")
+    @GetMapping("/viewRecipe")
+    public List<RecipeModel> viewRecipe(){
+    return (List<RecipeModel>) dao.findAll();
 }
 }
 
